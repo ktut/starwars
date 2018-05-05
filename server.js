ceba90e131e9089597a57a723f9b6146a6c2b181
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-05-05 13:19:54 -0500

    server changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 // Data
 var characters = [{
